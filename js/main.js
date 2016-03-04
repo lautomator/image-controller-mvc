c6ff4eff21323893a1cc8603873d7eca86d83240
John Merigliano <jmerigliano@gmail.com>
2016-03-04 17:34:08 -0500

    added --> - background image - front end and css - initial 'setter' functions (in progress) - readme

--- js/main.js
+++ js/main.js
@@ -1,24 +1,60 @@
 var data = {
-
-    // adjustable params
-    brightness: 10, // 0 - 10
-    contrast: 5, // 0 - 10
-    opacity: 80 // 0 - 100
+    /* This is the model. */
+    sourceImage: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Gin_and_Tonic_with_ingredients.jpg',
+    // default params
+    brightness: 50, // 0 - 100
+    contrast: 50, // 0 - 100
+    opacity: 10 // 0 - 10
 };
 
 var viewModel = {
-    // defines dynamic values
+    /* This is the controller. */
+
+    // adjustable user params
     siteData: ko.observableArray(),
     brightness: ko.observable(),
     contrast: ko.observable(),
     opacity: ko.observable(),
+    image: ko.observable(),
+
+    // note: only one filter will work at a time
+    setBrightness: function() {
+        /*
+            sets the brightness
+        */
+        console.log('brightness:', this.brightness());
+    },
+
+    setContrast: function() {
+        /*
+            sets the contrast
+        */
+        console.log('contrast:', this.contrast());
+    },
+
+    setOpacity: function() {
+        /*
+            sets the opacity
+        */
+
+        // Because CSS sets opacity as a floating
+        // point number bewteen 0 and 1 (ie: 0.5),
+        // we need to humanize that a bit for the UI.
+        // Therefore:
+        var opacity = this.opacity() / 10;
+
+        $('.source_image').css('opacity', opacity);
+    },
 
     init: function() {
-        // define the site data
-        this.siteData(data);
+        /*
+            initially defines the observables
+            with the default data from the model
+        */
         this.brightness(data.brightness);
         this.contrast(data.contrast);
         this.opacity(data.opacity);
+        this.image(data.sourceImage);
     }
 };
 
