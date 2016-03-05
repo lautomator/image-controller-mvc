5d49a8afb9e201f0c8165cc25ec460f61b949743
John Merigliano <jmerigliano@gmail.com>
2016-03-05 10:00:20 -0500

    additions -> - completed brightness function - TODO: contrast function

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var data = {
     /* This is the model. */
     sourceImage: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Gin_and_Tonic_with_ingredients.jpg',
     // default params
-    brightness: 50, // 0 - 100
-    contrast: 50, // 0 - 100
+    brightness: 0, // -10 - 10
+    contrast: 0, // -10 - 10
     opacity: 100, // 0 - 100
     grayscale: false
 };
@@ -12,41 +12,45 @@ var viewModel = {
     /* This is the controller. */
 
     // adjustable user params
-    siteData: ko.observableArray(),
     brightness: ko.observable(),
     contrast: ko.observable(),
     opacity: ko.observable(),
     grayscale: ko.observable(),
     image: ko.observable(),
 
-    // note: only one filter will work at a time
     setBrightness: function() {
         /*
             sets the brightness
         */
-        // set the opacity to 100%
+        // reset the opacity to 100%
         this.opacity(100);
         this.setOpacity();
 
-        // determine tint or shade
-        if (this.brightness() >= 50) {
-            // 1.) Make the white box visible at 0 opacity.
-            // 2.) As the value of the slider increases (this.brightness()),
-            //     increase the opacity of the white box.
-        } else {
-            // brightness is less than 50
-            // Add the black background
-            // lower of the image as the value of brightness decreases
-        }
+        var opacity;
 
-        console.log('brightness:', this.brightness());
+        // determine tint or shade:
+        // tint is > 0; shade is < 0
+        if (this.brightness() > 0) {
+            opacity = (10 - this.brightness()) * 0.1;
+
+            // set the background as white and fade the image
+            $('.image_holder').css('background', 'white');
+            $('.source_image').css('opacity', opacity);
+
+        } else {
+            opacity = 1 + (this.brightness() * 0.1);
+
+            $('.image_holder').css('background', 'black');
+            $('.source_image').css('opacity', opacity);
+        }
     },
 
     setContrast: function() {
         /*
             sets the contrast
         */
-        console.log('contrast:', this.contrast());
+        // TODO
+        console.log(this.contrast());
     },
 
     setOpacity: function() {
