26eb7e6eacde64d12433a6653af4dd4278f85079
John Merigliano <jmerigliano@gmail.com>
2016-03-07 16:15:19 -0500

    major revisions -> -removed contrast option -added coolnes and warmness options -updated layout -added conditions for resetting

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ var data = {
     sourceImage: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Gin_and_Tonic_with_ingredients.jpg',
     // default params
     brightness: 0, // -10 - 10
-    contrast: 0, // -10 - 10
+    coolness: 0, // 0 - 10
+    warmness: 0, // 0 - 10
     opacity: 100, // 0 - 100
-    grayscale: false
+    grayscale: false,
+    lastUsed: '' // the last setting adjusted
 };
 
 var viewModel = {
@@ -13,18 +15,20 @@ var viewModel = {
 
     // adjustable user params
     brightness: ko.observable(),
-    contrast: ko.observable(),
+    coolness: ko.observable(),
+    warmness: ko.observable(),
     opacity: ko.observable(),
     grayscale: ko.observable(),
     image: ko.observable(),
+    lastUsed: ko.observable(),
 
     setBrightness: function() {
         /*
             sets the brightness
         */
-        // reset the opacity to 100%
-        this.opacity(100);
-        this.setOpacity();
+
+        this.lastUsed('brightness');
+        this.resetOthers(this.lastUsed());
 
         var opacity;
 
@@ -46,33 +50,38 @@ var viewModel = {
         }
     },
 
-    setContrast: function() {
+    setCoolness: function() {
         /*
-            sets the contrast
+            sets the coolness
         */
-        // reset the opacity to 100%
-        this.opacity(100);
-        this.setOpacity();
 
-        var opacity;
+        this.lastUsed('coolness');
+        this.resetOthers(this.lastUsed());
 
-        // determine degree of contrast:
-        if (this.contrast() > 0) {
-            opacity = (10 - this.contrast()) * 0.1;
+        var opacity = (10 - this.coolness()) * 0.1;
 
-            // set the background as blue and fade the image
-            // This is not really contrast but is only being
-            // used for the purposes of the demo.
-            $('.image_holder').css('background', 'blue');
-            $('.source_image').css('opacity', opacity);
+        // set the background as blue and fade the image
+        // This is not really coolness but is only being
+        // used for the purposes of the demo.
+        $('.image_holder').css('background', 'blue');
+        $('.source_image').css('opacity', opacity);
+    },
 
-        } else {
-            opacity = 1 + (this.contrast() * 0.1);
+    setWarmness: function() {
+        /*
+            sets the warmness
+        */
 
-            // set the background as gray and fade the image
-            $('.image_holder').css('background', 'gray');
-            $('.source_image').css('opacity', opacity);
-        }
+        this.lastUsed('warmness');
+        this.resetOthers(this.lastUsed());
+
+        var opacity = (10 - this.warmness()) * 0.1;
+
+        // set the background as blue and fade the image
+        // This is not really warmness but is only being
+        // used for the purposes of the demo.
+        $('.image_holder').css('background', 'orange');
+        $('.source_image').css('opacity', opacity);
     },
 
     setOpacity: function() {
@@ -80,17 +89,14 @@ var viewModel = {
             sets the opacity
         */
 
+        this.lastUsed('opacity');
+        this.resetOthers(this.lastUsed());
+
         // Because CSS sets opacity as a floating
         // point number bewteen 0 and 1 (ie: 0.5),
         // we need to humanize that a bit for the UI.
         // Therefore:
         var opacity = this.opacity() / 100;
-
-        // reset other function
-        // this.brightness(0);
-        // this.setBrightness();
-        // this.contrast(0);
-        // this.setContrast();
 
         // reset the background
         $('.image_holder').css('background', 'none');
@@ -101,6 +107,9 @@ var viewModel = {
         /*
             set to grayscale
         */
+
+        this.lastUsed('grayscale');
+
         if (!this.grayscale()) {
             $('.source_image').addClass('is_grayscale');
             this.grayscale(true);
@@ -110,16 +119,45 @@ var viewModel = {
         }
     },
 
+    resetOthers: function(last) {
+        /*
+            Resets all of the settings
+            except for the current setting.
+            Does not influence grayscale.
+        */
+
+        if (last !== 'opacity') {
+            this.opacity(100);
+        } else {
+            this.brightness(0);
+            this.coolness(0);
+            this.warmness(0);
+        }
+
+        if (last === 'brightness') {
+            this.coolness(0);
+            this.warmness(0);
+        } else if (last === 'warmness') {
+            this.brightness(0);
+            this.coolness(0);
+        } else {
+            this.brightness(0);
+            this.warmness(0);
+        }
+    },
+
     init: function() {
         /*
             initially defines the observables
             with the default data from the model
         */
         this.brightness(data.brightness);
-        this.contrast(data.contrast);
+        this.coolness(data.coolness);
+        this.warmness(data.warmness);
         this.opacity(data.opacity);
         this.grayscale(data.grayscale);
         this.image(data.sourceImage);
+        this.lastUsed(data.lastUsed);
     }
 };
 
